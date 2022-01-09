ae13f5bfb5261906057c4d9aaebf84936ad4cc54
Dzmitry Kavaleuski <dzmitry.kavaleuski@sqilsoft.by>
2022-01-09 19:19:37 +0300

    add frontend-angular

--- vocabilary/card/serializer.py
+++ vocabilary/card/serializer.py
@@ -5,4 +5,4 @@ from .models import Card
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
-        fields = ['english', 'russian', 'new_card', 'remembered', 'part_of_speech', 'repeat']
+        fields = ['pk', 'english', 'russian', 'new_card', 'remembered', 'part_of_speech', 'repeat']
